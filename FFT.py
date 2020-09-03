45abdd4cb3af8fcd897ca669a7f2df1c33876220
fraKICcodeur <francois.altieri@gmail.com>
2020-09-03 19:09:45 +0200

    add documentation and unit tests

--- FFT.py
+++ FFT.py
@@ -2,8 +2,17 @@ import numpy as np
 
 
 class FFTAnalyser:
+    """
+    helper class to perform Fast Fourier Transform on a signal across a moving window
+    provides a more advanced analysis than a simple moving average
+    """
     _dt = 0.1
     _window = 1
+
+    def set_timestep(self,timestep_seconds):
+        """ set the timestep in second used to sample the signal
+        """
+        self._dt = timestep_seconds
 
     def set_moving_average_window(self,n_seconds):
         """ set the duration in seconds of the window used for moving average
@@ -13,16 +22,32 @@ class FFTAnalyser:
         return self.compute_frequencies()
 
     def compute_frequencies(self):
+        """
+        :return: list of frequencies that are retrived with fft analysis
+        """
         freq = np.fft.rfftfreq(self.window_number_points(), self._dt)
         return freq
 
     def nyquist(self):
+        """
+        returns the NyQuist frequency for this analyser
+        any signal with a higher frequency cannot be analysed
+        """
         return 0.5/self._dt
 
     def window_number_points(self):
+        """
+        computes the number of values included in the window for moving average (ie window/timestep)
+        """
         return int(self._window / self._dt)
 
     def process_signal(self,signal):
+        """
+        analyse the given signal with a moving FFT across its duration
+        :param signal: signal to analyse (numpy 1D array)
+        :return: np array for all rffts with the moving window for r= process_signal(t) r[0] is the evolution of harmonic 0 over time
+        see compute_frequencies to know which frequency corresponds to a given index
+        """
         signal_size = len(signal)
         res = np.zeros((int(signal_size-self.window_number_points()),int(1+self.window_number_points()/2)))
         for i in range(0,signal_size-self.window_number_points()):
@@ -32,20 +57,9 @@ class FFTAnalyser:
         return res.transpose()
 
     def process_signal_index(self,signal,index):
+        """
+        same as process_signal but returns only the time evolution of the frequency at the given index
+        """
         res = self.process_signal(signal)
         return res[index]
 
-    @staticmethod
-    def generate_test_signal(duration, dt):
-        time = np.arange(0,duration,dt)
-        com = lambda f: np.cos(2*np.pi*f*time)
-        sigArray = com(1) + 0.5 * com(2)
-        return sigArray
-
-    @staticmethod
-    def generate_test_signal2(duration, dt):
-        time = np.arange(0,duration,dt)
-        com = lambda f: np.cos(2*np.pi*f*time)
-        sigArray = com(1) + 0.5 * com(2)
-        sigArray2 = com(1) + 0.5 * com(3)
-        return np.concatenate((sigArray,sigArray2))
--- test_FFT.py
+++ test_FFT.py
@@ -3,17 +3,70 @@ from FFT import *
 
 
 class TestFFTAnalyser(TestCase):
+
+    @staticmethod
+    def _generate_test_signal(duration, dt):
+        """
+        generate a simple signal equal to the sum of two cosine
+        result signal is at f = 1Hz and 2Hz
+        :param duration: duration of the generated signal
+        :param dt: timestep
+        :return: cos(2*pi*1Hz*t)+0.5*cos(2*pi*2Hz*t)
+        """
+        time = np.arange(0, duration, dt)
+        com = lambda f: np.cos(2 * np.pi * f * time)
+        sigArray = com(1) + 0.5 * com(2)
+        return sigArray
+
+    @staticmethod
+    def _generate_test_signal2(duration, dt):
+        """
+        generate a simple signal equal to the sum of two cosine
+        result signal is at f = 1Hz and 2Hz for t = 0:duration
+                            f = 1Hz and 3Hz for t = duration:2*duration
+        :param duration: duration of the generated signal
+        :param dt: timestep
+        :return: cos(2*pi*1Hz*t)+0.5*cos(2*pi*2Hz*t) t<= duration
+                 cos(2*pi*1Hz*t)+0.5*cos(2*pi*3Hz*t) t> duration
+        """
+        time = np.arange(0, duration, dt)
+        com = lambda f: np.cos(2 * np.pi * f * time)
+        sigArray = com(1) + 0.5 * com(2)
+        sigArray2 = com(1) + 0.5 * com(3)
+        return np.concatenate((sigArray, sigArray2))
+
     def test_compute_frequencies(self):
         f = FFTAnalyser()
-        freqs = f.compute_frequencies()
+        f.set_timestep(0.1)
+        freqs = f.set_moving_average_window(1)
         nyquist = f.nyquist()
         self.assertGreaterEqual(nyquist, np.max(freqs))
 
+    def test_process_signal(self):
         dt = 0.1
-        time = lambda i: i * dt
-        twoPiT = lambda i: time(i) * 2 * np.pi
-        omegaTForF = lambda i,f : f*twoPiT(i)
-        com = lambda i, f: np.cos(omegaTForF(i, f))
-        signal = lambda i: com(i, 1) + 0.5 * com(i, 2)
-        sigArray = np.fromfunction(signal, (300,))
-        f.process_signal(sigArray)
+        duration = 10
+        f = FFTAnalyser()
+        f.set_timestep(dt)
+        freqs = f.set_moving_average_window(1)
+        signal2 = TestFFTAnalyser._generate_test_signal2(duration, dt)
+        r = f.process_signal(signal2)
+        average = r[0]
+        self.assertAlmostEqual(average[0], 0)
+        freq1 = r[1]
+        self.assertNotAlmostEqual(freq1[0], 0)
+        freq2 = r[2]
+        # makes sure that 2Hz is found at the beginning of the signal but not at the end
+        self.assertNotAlmostEqual(freq2[0], 0)
+        self.assertAlmostEqual(freq2[len(freq2)-1], 0)
+
+    def test_process_signal_index(self):
+        dt = 0.1
+        duration = 10
+        f = FFTAnalyser()
+        f.set_timestep(dt)
+        f.set_moving_average_window(1)
+        signal = TestFFTAnalyser._generate_test_signal(duration, dt)
+        r = f.process_signal(signal)
+        average = r[0]
+        average_by_index = f.process_signal_index(signal,0)
+        self.assertEqual(average.all(),average_by_index.all())
