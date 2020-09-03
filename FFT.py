1bedd30e713b0a6aa2f0d85bfec80c755cd8ee14
fraKICcodeur <francois.altieri@gmail.com>
2020-09-03 18:43:20 +0200

    finish up some cleaning FFT can now perform required averages

--- FFT.py
+++ FFT.py
@@ -5,6 +5,13 @@ class FFTAnalyser:
     _dt = 0.1
     _window = 1
 
+    def set_moving_average_window(self,n_seconds):
+        """ set the duration in seconds of the window used for moving average
+            returns the list of frequencies that can be analysed with that window
+        """
+        _window = n_seconds
+        return self.compute_frequencies()
+
     def compute_frequencies(self):
         freq = np.fft.rfftfreq(self.window_number_points(), self._dt)
         return freq
@@ -28,13 +35,15 @@ class FFTAnalyser:
         res = self.process_signal(signal)
         return res[index]
 
-    def generate_test_signal(self,duration, dt):
+    @staticmethod
+    def generate_test_signal(duration, dt):
         time = np.arange(0,duration,dt)
         com = lambda f: np.cos(2*np.pi*f*time)
         sigArray = com(1) + 0.5 * com(2)
         return sigArray
 
-    def generate_test_signal2(self,duration, dt):
+    @staticmethod
+    def generate_test_signal2(duration, dt):
         time = np.arange(0,duration,dt)
         com = lambda f: np.cos(2*np.pi*f*time)
         sigArray = com(1) + 0.5 * com(2)
